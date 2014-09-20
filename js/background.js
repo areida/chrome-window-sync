33ee8e152cc7864cd4d17ff3420f6f5f095814d5
Andrew <andrew@syn0.com>
2014-09-19 20:54:30 -0700

    Better session memory

--- js/background.js
+++ js/background.js
@@ -1,4 +1,5 @@
-var masterTab;
+var ACTIVE_IMAGE_PATH   = 'images/icon-128x128-inverse.png';
+var INACTIVE_IMAGE_PATH = 'images/icon-128x128.png';
 
 function syncTabs(tab) {
     chrome.tabs.query({active : true}, function(tabs) {
@@ -16,38 +17,54 @@ function syncTabs(tab) {
     });
 }
 
-function masterTabListener(tabId, changeInfo, tab) {
-    if (
-        tabId === masterTab.id &&
-        changeInfo.status &&
-        changeInfo.status === 'complete'
-    ) {
-        syncTabs(tab);
-    }
+// Trigger other tabs to sync when the master tab updates
+function tabUpdateListener(tabId, changeInfo, tab) {
+    chrome.storage.local.get('tab', function(values) {
+        if (
+            values.tab &&
+            tabId === values.tab.id &&
+            changeInfo.status &&
+            changeInfo.status === 'complete'
+        ) {
+            syncTabs(tab);
+        }
+    });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    masterTab = tab;
+// Check initial state
+chrome.storage.local.get({
+    tab    : null,
+    toggle : false
+}, function(values) {
 
-    chrome.storage.local.get({toggle : false}, function(values) {
-        if (! values.toggle) {
-            chrome.storage.local.set({toggle : true}, function() {
-                chrome.tabs.onUpdated.addListener(masterTabListener)
+    // If the script is activated
+    if (values.tab && values.toggle) {
+        syncTabs(values.tab);
+        chrome.tabs.onUpdated.addListener(tabUpdateListener)
+        chrome.browserAction.setIcon({path : ACTIVE_IMAGE_PATH});
+    }
 
-                syncTabs(masterTab);
-
-                chrome.browserAction.setIcon({
-                    path : 'images/icon-128x128-inverse.png'
+    chrome.browserAction.onClicked.addListener(function(tab) {
+        chrome.storage.local.get({
+            toggle : values.toggle
+        }, function(values) {
+            // If the script is active, deactivate it
+            if (values.toggle) {
+                chrome.tabs.onUpdated.removeListener(tabUpdateListener);
+                chrome.browserAction.setIcon({path : INACTIVE_IMAGE_PATH});
+                chrome.storage.local.set({
+                    tab    : null,
+                    toggle : false
                 });
-            });
-        } else {
-            chrome.storage.local.set({toggle : false}, function() {
-                chrome.tabs.onUpdated.removeListener(masterTabListener);
-
-                chrome.browserAction.setIcon({
-                    path : 'images/icon-128x128.png'
+            } else {
+                syncTabs(tab);
+                chrome.tabs.onUpdated.addListener(tabUpdateListener);
+                chrome.browserAction.setIcon({path : ACTIVE_IMAGE_PATH});
+                chrome.storage.local.set({
+                    tab    : tab,
+                    toggle : true
                 });
-            });
-        }
+            }
+        });
     });
 });
