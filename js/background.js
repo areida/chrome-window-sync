b50eed67027983334e214cbc33a5ebddc51e9f5e
Andrew <andrew@syn0.com>
2014-09-20 12:09:51 -0700

    Sending click events to open window active tabs

--- js/background.js
+++ js/background.js
@@ -1,80 +1,129 @@
 var ACTIVE_IMAGE_PATH   = 'images/icon-128x128-inverse.png';
 var INACTIVE_IMAGE_PATH = 'images/icon-128x128.png';
 
-function updateHistoryFromTab(tab) {
+// Performs a callback on each active tab that isn't the master tab
+function eachActiveTab(masterTab, callback) {
     chrome.tabs.query({active : true}, function(tabs) {
-        var changeUrl, reload;
-
         for (var i in tabs) {
-            if (tabs[i].id !== tab.id) {
-                changeUrl = 'history.pushState({}, \'\', \''+tab.url+'\');';
-                reload    = 'parent.location.hash=\'!\';';
-
-                chrome.tabs.executeScript(tabs[i].id, {code : changeUrl});
-                chrome.tabs.executeScript(tabs[i].id, {code : reload});
+            if (tabs[i].id !== masterTab.id) {
+                callback(tabs[i]);
             }
         }
     });
 }
 
-function updateUrlFromTab(tab) {
-    chrome.tabs.query({active : true}, function(tabs) {
-        for (var i in tabs) {
-            if (tabs[i].id !== tab.id) {
-                chrome.tabs.update(tabs[i].id, {url : tab.url});
-            }
+// Update functions
+function updateHistoryFromTab(masterTab) {
+    var changeUrl, reload;
+
+    changeUrl = 'history.pushState({}, \'\', \'' + masterTab.url + '\');';
+    reload    = 'parent.location.hash=\'!\';';
+
+    eachActiveTab(masterTab, function(tab) {
+        chrome.tabs.executeScript(tab.id, {code : changeUrl});
+        chrome.tabs.executeScript(tab.id, {code : reload});
+    });
+}
+
+function updateUrlFromTab(masterTab) {
+    eachActiveTab(masterTab, function(tab) {
+        chrome.tabs.update(tab.id, {url : masterTab.url});
+    });
+}
+
+function simulateClick(masterTab, request) {
+    var regex, click;
+
+    regex = request.targetClass.replace(' ', '.*');
+    click = (
+        'var elements = document.getElementsByTagName(\'' + request.tagName + '\');' +
+        'for(var i in elements) {' +
+            'if (elements[i].className.match(/' + regex + '/)) {' +
+                'elements[i].click();' +
+                'break;' +
+            '}' +
+        '}'
+    );
+
+    eachActiveTab(masterTab, function(tab) {
+        chrome.tabs.executeScript(tab.id, {code : click});
+    });
+}
+
+// Listener functions
+function browserActionListener(masterTab, toggle) {
+    chrome.storage.local.get({
+        toggle : toggle
+    }, function(values) {
+        // If the script is active, deactivate it
+        if (values.toggle) {
+            chrome.browserAction.setIcon({path : INACTIVE_IMAGE_PATH});
+            chrome.browserAction.setTitle({title : 'Sync windows'});
+            chrome.runtime.onMessage.removeListener();
+            chrome.tabs.onUpdated.removeListener();
+            chrome.storage.local.set({
+                tab    : null,
+                toggle : false
+            });
+        } else {
+            startListeners(masterTab);
+
+            chrome.storage.local.set({
+                tab    : masterTab,
+                toggle : true
+            });
         }
     });
 }
 
-function tabUpdateListener(tabId, changeInfo, tab) {
-    chrome.storage.local.get('tab', function(values) {
-        if (
-            values.tab &&
-            tabId === values.tab.id &&
-            changeInfo.status &&
-            changeInfo.status === 'complete'
-        ) {
-            updateHistoryFromTab(tab);
+function contentScriptListener(masterTab, request, sendResponse) {
+    if (request.eventType === 'click') {
+        simulateClick(masterTab, request)
+    }
+}
+
+function tabUrlListener(masterTab, tabId, changeInfo, changeTab) {
+    if (
+        tabId === masterTab.id &&
+        changeInfo.status &&
+        changeInfo.status === 'complete'
+    ) {
+        updateHistoryFromTab(changeTab);
+    }
+}
+
+// Start listeners on the master tab
+function startListeners(masterTab) {
+    updateUrlFromTab(masterTab);
+
+    chrome.browserAction.setIcon({path : ACTIVE_IMAGE_PATH});
+    chrome.browserAction.setTitle({title : 'Unsync windows'});
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (sender.tab.id === masterTab.id) {
+            contentScriptListener(masterTab, request, sendResponse);
         }
+    });
+
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, changeTab) {
+        tabUrlListener(masterTab, tabId, changeInfo, changeTab);
     });
 }
 
-// Check initial state
+// Initialize extension
 chrome.storage.local.get({
     tab    : null,
     toggle : false
 }, function(values) {
+    var browserActionClickListenerProxy;
 
-    // If the script is activated
+    // If the script is active restart the listeners
     if (values.tab && values.toggle) {
-        updateUrlFromTab(values.tab);
-        chrome.tabs.onUpdated.addListener(tabUpdateListener)
-        chrome.browserAction.setIcon({path : ACTIVE_IMAGE_PATH});
+        startListeners(values.tab);
     }
 
+    // Listen for user clicks on browser action icon
     chrome.browserAction.onClicked.addListener(function(tab) {
-        chrome.storage.local.get({
-            toggle : values.toggle
-        }, function(values) {
-
-            // If the script is active, deactivate it
-            if (values.toggle) {
-                chrome.tabs.onUpdated.removeListener(tabUpdateListener);
-                chrome.browserAction.setIcon({path : INACTIVE_IMAGE_PATH});
-                chrome.storage.local.set({
-                    tab    : null,
-                    toggle : false
-                });
-            } else {
-                updateUrlFromTab(tab);
-                chrome.tabs.onUpdated.addListener(tabUpdateListener);
-                chrome.browserAction.setIcon({path : ACTIVE_IMAGE_PATH});
-                chrome.storage.local.set({
-                    tab    : tab,
-                    toggle : true
-                });
-            }
-        });
+        browserActionListener(tab, values.toggle)
     });
 });
---
+++ js/content.js
@@ -0,0 +1,19 @@
+document.addEventListener('click', function(event) {
+    var attributes = event.target.attributes;
+
+    for (var i in attributes) {
+        if (
+            attributes[i].localName === 'href' &&
+            attributes[i].value &&
+            attributes[i].value !== 'javascript:;'
+        ) {
+            return;
+        }
+    }
+
+    chrome.runtime.sendMessage({
+        eventType   : 'click',
+        tagName     : event.target.tagName,
+        targetClass : event.target.className
+    });
+}, true);
