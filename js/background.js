8dc7004f5dd6d5e9064f62d66c33725d5a377f05
Andrew <andrew@syn0.com>
2014-09-19 20:29:17 -0700

    Toggling icon when active

--- js/background.js
+++ js/background.js
@@ -1,21 +1,53 @@
-function syncTabs(newTab, masterTab) {
-    masterTab = masterTab || newTab;
+var masterTab;
 
+function syncTabs(tab) {
     chrome.tabs.query({active : true}, function(tabs) {
+        var changeUrl, reload;
+
         for (var i in tabs) {
-            if (tabs[i].id !== masterTab.id) {
-                chrome.tabs.update(tabs[i].id, {url: newTab.url});
+            if (tabs[i].id !== tab.id) {
+                changeUrl = 'history.pushState({}, \'\', \''+tab.url+'\');';
+                reload    = 'parent.location.hash=\'!\';';
+
+                chrome.tabs.executeScript(tabs[i].id, {code : changeUrl});
+                chrome.tabs.executeScript(tabs[i].id, {code : reload});
             }
         }
     });
 }
 
-chrome.browserAction.onClicked.addListener(function(masterTab) {
-    syncTabs(masterTab);
+function masterTabListener(tabId, changeInfo, tab) {
+    if (
+        tabId === masterTab.id &&
+        changeInfo.status &&
+        changeInfo.status === 'complete'
+    ) {
+        syncTabs(tab);
+    }
+}
 
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, newTab) {
-        if (tabId === masterTab.id && changeInfo.status && changeInfo.status === 'complete') {
-            syncTabs(newTab, masterTab);
+chrome.browserAction.onClicked.addListener(function(tab) {
+    masterTab = tab;
+
+    chrome.storage.local.get({toggle : false}, function(values) {
+        if (! values.toggle) {
+            chrome.storage.local.set({toggle : true}, function() {
+                chrome.tabs.onUpdated.addListener(masterTabListener)
+
+                syncTabs(masterTab);
+
+                chrome.browserAction.setIcon({
+                    path : 'images/icon-128x128-inverse.png'
+                });
+            });
+        } else {
+            chrome.storage.local.set({toggle : false}, function() {
+                chrome.tabs.onUpdated.removeListener(masterTabListener);
+
+                chrome.browserAction.setIcon({
+                    path : 'images/icon-128x128.png'
+                });
+            });
         }
     });
 });
